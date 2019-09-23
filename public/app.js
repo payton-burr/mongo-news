fb6dd2bbd9310fda7f7f8539dbca1dde9044f0f1
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2019-09-23 17:21:39 -0600

    changed styling, removed console.logs

--- public/app.js
+++ public/app.js
@@ -13,8 +13,7 @@ $(document).on("click", "p", function() {
     url: "/articles/" + thisId
   })
     .then(function(data) {
-      console.log(data);
-      $("#notes").append("<h2>" + data.title + "</h2>");
+      $("#notes").append("<h4>" + data.title + "</h4>");
       $("#notes").append("<input id='titleinput' name='title' >");
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
